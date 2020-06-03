ad8c35636f183fa6b473665c8503a7c89e610f4a
hyang66 <mistyfeather1@gmail.com>
2020-06-03 15:25:19 -0400

    minor bug fixes

--- generate.py
+++ generate.py
@@ -52,47 +52,55 @@ def check_valid(coords, number, board):
 
 # generate filled 2x2 boards
 def generate_board():
-    board = [[0 for i in range(4)] for i in range(4)]
 
     # probably not optimal:
     # - generate 1 row at a time, and keep track of what is allowed
+    redo = True # try to generate again
+    while redo:
+        board = [[0 for i in range(4)] for i in range(4)]
+        redo = False
     
-    for row_num in range(len(board)):
-        for col_num in range(len(board)):
-            if row_num == 0: # for the first row, doesnt matter what we do
-                board[row_num] = ["1", "2", "3", "4"]
-                random.shuffle(board[row_num])
-            else: 
-                order = ["1", "2", "3", "4"]
-                random.shuffle(order)
-                for num in order:
-                    coords = (row_num, col_num)
-                    if check_valid(coords, num, board):
-                        board[row_num][col_num] = num
-                        break
-
-                
+        for row_num in range(len(board)):
+            for col_num in range(len(board)):
+                if row_num == 0: # for the first row, doesnt matter what we do
+                    board[row_num] = ["1", "2", "3", "4"]
+                    random.shuffle(board[row_num])
+                else: 
+                    order = ["1", "2", "3", "4"]
+                    random.shuffle(order) # shuffle the order we try the numbers in
+                    for num in order:
+                        coords = (row_num, col_num)
+                        if check_valid(coords, num, board):
+                            board[row_num][col_num] = num
+                            break
+                    # we chose a bad order of numbers
+                    if board[row_num][col_num] == 0:
+                        redo = True
 
     return board
 
 # remove numbers
 def remove_nums(board):
-    next_to_remove = [i for i in range(16)]
-    random.shuffle(next_to_remove)
-    # randomly try removing things in specific orders
-    for next_ in next_to_remove:
-        row = next_ // 4
-        col = next_ % 4
-        coords = (row, col)
-        curr_num = board[row][col]
-        removable = True
-        # check to see if it can be replace with another number
-        # if it can, then no unique solution = bad
-        for number in range(1,5):
-            if number != curr_num and check_valid(coords, number, board):
-                removable = False
-        if removable:
-            board[row][col] = "_"
+    more_to_remove = True
+    while more_to_remove: # keep trying to remove things until nothing can be
+        next_to_remove = [i for i in range(16)]
+        random.shuffle(next_to_remove)
+        # randomly try removing things in specific orders
+        for next_ in next_to_remove:
+            row = next_ // 4
+            col = next_ % 4
+            coords = (row, col)
+            curr_num = board[row][col]
+            removable = True
+            more_to_remove = False
+            # check to see if it can be replace with another number
+            # if it can, then no unique solution = bad
+            for number in range(1,5):
+                if number != curr_num and check_valid(coords, number, board):
+                    removable = False
+            if removable:
+                board[row][col] = "_"
+                more_to_remove = True
     return board
 
 def disp_board(board):
@@ -111,3 +119,10 @@ disp_board(remove_nums(b))
                    # [3,4,0,0],
                    # [0,0,0,0],
                    # [0,0,0,0]]))
+
+# print(check_valid( (1,3), 3,
+# [['2', '1', '4', '3'],
+# ['2', '4', '1', '3'],
+# ['1', '2', '3', '4'],
+# ['4', '3', '2', '1']]
+# ))
